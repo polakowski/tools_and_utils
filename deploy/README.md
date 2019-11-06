97fd4b8e541e56b0c6e1f9c71e114dd7ddfd2115
Marek Polakowski <marek.polakowski@gmail.com>
2019-11-06 23:06:06 +0100

    color output; add deploy script for git/git

---
+++ deploy/README.md
@@ -0,0 +1,4 @@
+## Deploy directory
+Contains bash scripts to automatically deploy helper scripts from repository folder.
+
+⚠️ Use with caution.
--- git/git.sh
+++ git/git.sh
@@ -21,22 +21,33 @@ alias amendcommit='git commit --amend --no-edit'
 alias fetch='echo "Fetching branches..."; git fetch --quiet && echo "Done!"'
 
 commit() {
-  git commit -m $@ && echo '[Commit] Committed plain message.'
+  _raw_commit "$@"
 }
 
 tcommit() {
   task_id=$(git symbolic-ref --short HEAD | cut -d '_' -f 1)
-  git commit -m "$task_id $@" && echo '[Commit] Committed with task number.'
+  msg="$task_id $@"
+
+  _raw_commit "$msg"
 }
 
 fcommit() {
   task_id=$(git symbolic-ref --short HEAD | cut -d '_' -f 1 | grep -Eo '\d+')
-  git commit -m "$task_id: $@" && echo '[Commit] Committed with task number.'
+  msg="$task_id $@"
+
+  _raw_commit "$msg"
+}
+
+commithf() {
+  if [ "$#" -eq 0 ]; then
+    commit '[hotfix]'
+  else
+    commit "[hotfix] $@"
+  fi
 }
 
 ggpullremote() {
-  if [ -z $1 ]
-  then
+  if [ -z $1 ]; then
     echo 'Please pass branch name (example: "ggpullremote feature-branch")'
     return
   fi
@@ -50,8 +61,7 @@ ggpullremote() {
 }
 
 ggrebase() {
-  if [ -z $1 ]
-  then
+  if [ -z $1 ]; then
     echo 'Please pass branch name (example: "ggrebase master")'
     return
   fi
@@ -71,8 +81,7 @@ ggrebase() {
   n_conflicts=$(git diff --name-only --diff-filter=U | wc -l | tr -d '[:space:]')
 
   # handle conflicts
-  if [ "$n_conflicts" == "0" ]
-  then
+  if [ "$n_conflicts" == "0" ]; then
     echo 'Successfully rebased.'
   else
     echo "$n_conflicts conflicted files found."
@@ -83,19 +92,16 @@ ggresolve() {
   text_editor=$P_EDITOR_NAME
   n_conflicts=$(git diff --name-only --diff-filter=U | wc -l | tr -d '[:space:]')
 
-  if [ "$n_conflicts" == "0" ]
-  then
+  if [ "$n_conflicts" == "0" ]; then
     echo 'No conflicts found.'
     return
   fi
 
-  if [ ! -z $1 ]
-  then
+  if [ ! -z $1 ]; then
     text_editor=$1
   fi
 
-  if [ -z $text_editor ]
-  then
+  if [ -z $text_editor ]; then
     echo 'Please pass text editor name (example: "ggresolve vim")'
     echo 'TIP: Editor name defaults to `P_EDITOR_NAME` variable.'
     return
@@ -108,4 +114,42 @@ ggresolve() {
 
 gcoo() {
   git checkout $(git branch | awk '/^[^\*]/ { print $1 }' | fzf)
+}
+
+# UTILS -----------------------------------------------------------------------
+
+COLOR_RED='1'
+COLOR_YELLOW='3'
+COLOR_BLUE='4'
+
+_raw_commit() {
+  _check_len "$@" 50 && git commit --quiet -m "$@" && _commit_echo "$@"
+}
+
+_check_len() {
+  str=$1
+  max_length=$2
+
+  str_length=${#str}
+
+  if [[ "$str_length" -gt "$max_length" ]]; then
+    _error_echo "Error: Commit message too long (max: $max_length characters)"
+    return 1
+  fi
+}
+
+_echo_color() {
+  tput setaf $COLOR_BLUE
+  echo -ne 'commit '
+  tput setaf "$1"
+  echo "$2"
+  tput sgr0
+}
+
+_commit_echo() {
+  _echo_color $COLOR_YELLOW "$@"
+}
+
+_error_echo() {
+  _echo_color $COLOR_RED "$@"
 }
